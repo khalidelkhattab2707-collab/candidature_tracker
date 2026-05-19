02aab0265f24e9d93eb99fad3ba91ef0949b8e57
khalid.elkhattab2707-collab <khalid.elkhattab2707@gmail.com>
2026-05-19 13:10:47 +0100

    creation des modules et migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,7 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function candidatures():HasMany{
+        return $this->hasmany(Candidature::class);
+    }
 }
